f2567d6ea778c31d57fead71ec053e7acf24b1bb
Ilya Solovyov <astronaut.str@gmail.com>
2015-10-19 16:14:53 +0300

    Initial Commit

---
+++ AcidSS/AcidSSView.h
@@ -0,0 +1,13 @@
+//
+//  AcidSSView.h
+//  AcidSS
+//
+//  Created by Ilya Solovyov on 19/10/15.
+//  Copyright © 2015 Ilya Solovyov. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface AcidSSView : ScreenSaverView
+
+@end
